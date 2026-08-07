16adc5f2f4d03862cae2ebfee88becb417fce06a
Jeff Rogers <redbeardcodes@gmail.com>
2026-08-07 16:36:47 -0400

    Update and refactor JdbcTemplate to DRY it up and pass QueryException instead of random SQLExceptions.

--- src/main/java/query4j/JdbcTemplate.java
+++ src/main/java/query4j/JdbcTemplate.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import query4j.exceptions.QueryException;
+
 public class JdbcTemplate {
 	private final DataSource ds;
 
@@ -17,8 +19,8 @@ public class JdbcTemplate {
 	}
 
 	public <T> List<T> query(String sql, RowMapper<T> mapper, Object... params) {
-		try (Connection conn = ds.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
-			bind(ps, params);
+		try (Connection conn = ds.getConnection()) {
+			PreparedStatement ps = prepareStatement(conn, sql, params);
 
 			try (ResultSet rs = ps.executeQuery()) {
 				List<T> results = new ArrayList<>();
@@ -27,12 +29,27 @@ public class JdbcTemplate {
 				return results;
 			}
 		} catch (SQLException sqle) {
-			throw new RuntimeException(sqle);
+			throw new QueryException(sqle);
+		}
+	}
+
+	public int update(String sql, Object... params) {
+		try (Connection conn = ds.getConnection()) {
+			PreparedStatement ps = prepareStatement(conn, sql, params);
+			return ps.executeUpdate();
+		} catch (SQLException sqle) {
+			throw new QueryException(sqle);
 		}
 	}
 
 	private void bind(PreparedStatement ps, Object[] params) throws SQLException {
 		for (int i = 0; i < params.length; i++)
 			ps.setObject(i + 1, params[i]);
+	}
+
+	private PreparedStatement prepareStatement(Connection conn, String sql, Object... params) throws SQLException {
+		PreparedStatement ps = conn.prepareStatement(sql);
+		bind(ps, params);
+		return ps;
 	}
 }
