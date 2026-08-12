2f41e0eba1c6553d203e3bce7b40a8b228c9c90e
Jeff Rogers <redbeardcodes@gmail.com>
2026-08-12 11:54:52 -0400

    Finish API move and clean up JdbcTest even more.

--- src/test/java/query4j/JdbcTest.java
+++ src/test/java/query4j/JdbcTest.java
@@ -12,12 +12,11 @@ import java.util.UUID;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
-public class JdbcTemplateTest {
+public class JdbcTest {
 
 	@Test
-	public void testThat_JdbcTemplate_query_successfullyReturns_expectedValues() throws Exception {
-		Db db = getJdbcTemplate();
-		JdbcTemplate jdbc = db.jdbc();
+	public void testThat_Jdbc_query_successfullyReturns_expectedValues() throws Exception {
+		Jdbc jdbc = getJdbc();
 		List<User> users = jdbc.query("select * from fake_users", User.MAPPER);
 		assertFalse(users.isEmpty(), "Users value is empty");
 		assertEquals("olivia", users.get(0).firstName, "user firstName does not match");
@@ -27,9 +26,8 @@ public class JdbcTemplateTest {
 	}
 
 	@Test
-	public void testThat_JdbcTemplate_update_successfullyUpdatesRow() throws Exception {
-		Db db = getJdbcTemplate();
-		JdbcTemplate jdbc = db.jdbc();
+	public void testThat_Jdbc_update_successfullyUpdatesRow() throws Exception {
+		Jdbc jdbc = getJdbc();
 
 		String firstName = "olivia";
 		String oldLastName = "dunham";
@@ -46,21 +44,25 @@ public class JdbcTemplateTest {
 		User updated = jdbc.queryOne("select * from fake_users where first_name = ?", User.MAPPER, firstName);
 		assertTrue(newLastName.equals(updated.lastName()),
 				"The expected lastName value is not correct after update");
+	}
+
+	@Test
+	public void testThat_Jdbc_query_and_update_methodCalls_throwQueryException() throws Exception {
 
 	}
 
-	private Db getJdbcTemplate() throws Exception {
+	private Jdbc getJdbc() throws Exception {
 		JdbcDataSource ds = new JdbcDataSource();
 		UUID uuid = UUID.randomUUID();
 		ds.setURL("jdbc:h2:mem:" + uuid + ";DB_CLOSE_DELAY=-1");
 		ds.setUser("sa");
 		ds.setPassword("");
-		JdbcTemplate template = new JdbcTemplate(ds);
+		Jdbc jdbc = new Jdbc(ds);
 
 		try (Connection conn = ds.getConnection(); Statement stmt = conn.createStatement()) {
-			stmt.execute(Sql.resource("sql/JdbcTemplateTest/01_setup_query.sql"));
+			stmt.execute(Sql.resource("sql/JdbcTest/01_setup_query.sql"));
 		}
-		return new Db(ds, template);
+		return jdbc;
 	}
 
 	record User(String firstName, String lastName, String email) {
@@ -68,8 +70,5 @@ public class JdbcTemplateTest {
 				rs.getString("first_name"),
 				rs.getString("last_name"),
 				rs.getString("email"));
-	}
-
-	record Db(JdbcDataSource ds, JdbcTemplate jdbc) {
 	}
 }
