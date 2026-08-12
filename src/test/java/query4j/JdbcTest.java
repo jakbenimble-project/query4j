293ef7dd7ceb85dcfcfd900ed8b06d9de9246bb6
Jeff Rogers <redbeardcodes@gmail.com>
2026-08-12 14:37:11 -0400

    Add methods to ensure connection failures to check for QueryExceptions and check for thrown exceptions in unit test.

--- src/test/java/query4j/JdbcTest.java
+++ src/test/java/query4j/JdbcTest.java
@@ -3,6 +3,7 @@ package query4j;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.sql.Connection;
 import java.sql.Statement;
@@ -12,11 +13,13 @@ import java.util.UUID;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import query4j.exceptions.QueryException;
+
 public class JdbcTest {
 
 	@Test
 	public void testThat_Jdbc_query_successfullyReturns_expectedValues() throws Exception {
-		Jdbc jdbc = getJdbc();
+		Jdbc jdbc = getJdbcKeepOpen();
 		List<User> users = jdbc.query("select * from fake_users", User.MAPPER);
 		assertFalse(users.isEmpty(), "Users value is empty");
 		assertEquals("olivia", users.get(0).firstName, "user firstName does not match");
@@ -27,7 +30,7 @@ public class JdbcTest {
 
 	@Test
 	public void testThat_Jdbc_update_successfullyUpdatesRow() throws Exception {
-		Jdbc jdbc = getJdbc();
+		Jdbc jdbc = getJdbcKeepOpen();
 
 		String firstName = "olivia";
 		String oldLastName = "dunham";
@@ -47,14 +50,39 @@ public class JdbcTest {
 	}
 
 	@Test
-	public void testThat_Jdbc_query_and_update_methodCalls_throwQueryException() throws Exception {
-
+	public void testThat_Jdbc_methodCalls_throwQueryException_onDatabaseEmpty()
+			throws Exception {
+		Jdbc jdbc = getJdbcAutoClose();
+		assertThrows(QueryException.class, () -> {
+			jdbc.query("select * from fake_users where first_name = ?", User.MAPPER);
+		});
+		assertThrows(QueryException.class, () -> {
+			jdbc.queryOne("select * from fake_users where first_name = ?", User.MAPPER, "peter");
+		});
+		assertThrows(QueryException.class, () -> {
+			jdbc.update("update fake_users set last_name = ? where first_name = ?", "foo", "peter");
+		});
+		assertThrows(QueryException.class, () -> {
+			jdbc.queryOptional("select first_name where last_name = ?", User.MAPPER, "olivia");
+		});
+		assertThrows(QueryException.class, () -> {
+			jdbc.queryValue("select first_name where last_name = ?", User.class, "olivia");
+		});
+		assertThrows(QueryException.class, () -> {
+			jdbc.batchUpdate("insert into fake_users (first_name, last_name, email) values (?, ?, ?)",
+					List.of(
+							new Object[] { "test", "one", "t1@example.com" },
+							new Object[] { "test", "two", "t2@example.com" }));
+		});
 	}
 
-	private Jdbc getJdbc() throws Exception {
+	private Jdbc getJdbc(boolean autoClose) throws Exception {
 		JdbcDataSource ds = new JdbcDataSource();
 		UUID uuid = UUID.randomUUID();
-		ds.setURL("jdbc:h2:mem:" + uuid + ";DB_CLOSE_DELAY=-1");
+		if (autoClose)
+			ds.setURL("jdbc:h2:mem:" + uuid);
+		else
+			ds.setURL("jdbc:h2:mem:" + uuid + ";DB_CLOSE_DELAY=-1");
 		ds.setUser("sa");
 		ds.setPassword("");
 		Jdbc jdbc = new Jdbc(ds);
@@ -63,6 +91,14 @@ public class JdbcTest {
 			stmt.execute(Sql.resource("sql/JdbcTest/01_setup_query.sql"));
 		}
 		return jdbc;
+	}
+
+	private Jdbc getJdbcKeepOpen() throws Exception {
+		return getJdbc(false);
+	}
+
+	private Jdbc getJdbcAutoClose() throws Exception {
+		return getJdbc(true);
 	}
 
 	record User(String firstName, String lastName, String email) {
